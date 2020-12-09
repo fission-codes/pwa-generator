8751799ba9147e76b1a2270be1840412b6a6464c
Brian Ginsburg <brian@brianginsburg.com>
2020-12-09 14:55:40 -0800

    Add default focus highlighting
    A bit more subtle than the elm-ui default and uses Fission purple.

--- src/Spa/Document.elm
+++ src/Spa/Document.elm
@@ -25,7 +25,22 @@ toBrowserDocument : Document msg -> Browser.Document msg
 toBrowserDocument doc =
     { title = doc.title
     , body =
-        [ Element.layout [ width fill, height fill ]
+        [ Element.layoutWith
+            { options =
+                [ focusStyle
+                    { borderColor = Just (rgb255 0 0 0)
+                    , backgroundColor = Nothing
+                    , shadow =
+                        Just
+                            { color = rgb255 100 70 250
+                            , offset = ( 0, 0 )
+                            , blur = 2
+                            , size = 1
+                            }
+                    }
+                ]
+            }
+            [ width fill, height fill ]
             (column [ width fill, height fill ] doc.body)
         ]
     }
