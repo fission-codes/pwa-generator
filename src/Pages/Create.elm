6c302644f9d7bb43f057a3000a7dc8b47d51c71e
Brian Ginsburg <brian@brianginsburg.com>
2020-12-15 07:19:35 -0800

    Add manifest editor to create page
    Add the manifest editor next to the manifest outputs. The colors are used to
    update the background and theme colors in the page and in shared.

--- src/Pages/Create.elm
+++ src/Pages/Create.elm
@@ -1,9 +1,15 @@
 module Pages.Create exposing (Model, Msg, Params, page)
 
-import Components.ManifestEditor as ManifestEditor exposing (ManifestEditor)
+import Api
+import Components.ManifestEditor as ManifestEditor exposing (Problem)
+import Components.ManifestOutputs as ManifestOutputs
 import Element exposing (..)
 import Element.Border as Border
 import Element.Font as Font
+import Html.Attributes exposing (manifest)
+import Json.Encode as Encode
+import Manifest exposing (Manifest)
+import Manifest.Color
 import Material.Icons.Outlined as MaterialIcons
 import Material.Icons.Types exposing (Coloring(..))
 import Session exposing (Session)
@@ -12,7 +18,7 @@ import Spa.Document exposing (Document)
 import Spa.Generated.Route as Route
 import Spa.Page as Page exposing (Page)
 import Spa.Url as Url exposing (Url)
-import UI.Colors as Colors
+import UI.Colors as Colors exposing (Colors)
 
 
 page : Page Params Model Msg
@@ -38,6 +44,9 @@ type alias Params =
 type alias Model =
     { session : Session
     , device : Device
+    , manifest : Manifest
+    , colors : Colors
+    , problems : List Problem
     }
 
 
@@ -45,6 +54,9 @@ init : Shared.Model -> Url Params -> ( Model, Cmd Msg )
 init shared { params } =
     ( { session = shared.session
       , device = shared.device
+      , manifest = Manifest.init
+      , colors = Colors.init
+      , problems = []
       }
     , Cmd.none
     )
@@ -55,19 +67,44 @@ init shared { params } =
 
 
 type Msg
-    = ReplaceMe
+    = UpdateManifest (List Problem) Manifest
+    | CopyToClipboard String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ReplaceMe ->
-            ( model, Cmd.none )
+        UpdateManifest problems manifest ->
+            ( { model
+                | manifest = manifest
+                , problems = problems
+                , colors = updateColors model.colors manifest
+              }
+            , Cmd.none
+            )
+
+        CopyToClipboard elemId ->
+            ( model
+            , Api.copyToClipboard (Encode.string elemId)
+            )
+
+
+updateColors : Colors -> Manifest -> Colors
+updateColors currentColors manifest =
+    { backgroundColor =
+        Maybe.withDefault currentColors.backgroundColor <|
+            Manifest.Color.fromHex manifest.backgroundColor
+    , themeColor =
+        Maybe.withDefault currentColors.themeColor <|
+            Manifest.Color.fromHex manifest.themeColor
+    , fontColor = Manifest.Color.contrast manifest.backgroundColor
+    , themeFontColor = Manifest.Color.contrast manifest.themeColor
+    }
 
 
 save : Model -> Shared.Model -> Shared.Model
 save model shared =
-    shared
+    { shared | colors = model.colors }
 
 
 load : Shared.Model -> Model -> ( Model, Cmd Msg )
@@ -95,13 +132,15 @@ view model =
     , body =
         [ case model.device.class of
             Phone ->
-                column [ width fill, paddingXY 10 20 ] []
+                column [ width fill, paddingXY 10 20 ]
+                    [ paragraph [ Font.center ] [ text "Please use a tablet or desktop computer to create manifests." ]
+                    ]
 
             Tablet ->
                 case model.device.orientation of
                     Portrait ->
-                        column [ width fill, paddingXY 10 20, spacing 20 ]
-                            [ viewEditorControls
+                        column [ width fill, paddingXY 10 20 ]
+                            [ paragraph [ Font.center ] [ text "Please use landscape mode to create a manifest." ]
                             ]
 
                     Landscape ->
@@ -111,7 +150,19 @@ view model =
                             , paddingXY 0 30
                             , spacing 30
                             ]
-                            [ viewEditorControls
+                            [ viewEditorControls model.manifest
+                            , row [ width fill, spacing 30 ]
+                                [ ManifestEditor.view
+                                    { manifest = model.manifest
+                                    , problems = model.problems
+                                    , colors = model.colors
+                                    , onUpdateManifest = UpdateManifest
+                                    }
+                                , ManifestOutputs.view
+                                    { manifest = model.manifest
+                                    , onCopyToClipboard = CopyToClipboard
+                                    }
+                                ]
                             ]
 
             _ ->
@@ -121,27 +172,39 @@ view model =
                     , paddingXY 0 30
                     , spacing 30
                     ]
-                    [ viewEditorControls
+                    [ viewEditorControls model.manifest
+                    , row [ width fill, spacing 30 ]
+                        [ ManifestEditor.view
+                            { manifest = model.manifest
+                            , problems = model.problems
+                            , colors = model.colors
+                            , onUpdateManifest = UpdateManifest
+                            }
+                        , ManifestOutputs.view
+                            { manifest = model.manifest
+                            , onCopyToClipboard = CopyToClipboard
+                            }
+                        ]
                     ]
         ]
     }
 
 
-viewEditorControls : Element Msg
-viewEditorControls =
+viewEditorControls : Manifest -> Element Msg
+viewEditorControls manifest =
     row
         [ width fill
         , Border.width 1
         , Border.color Colors.lightGray
         ]
         [ row [ alignRight, spacing 5, Font.color Colors.darkGray ]
-            [ el [] <|
+            [ el [ Font.color (Manifest.Color.contrast manifest.backgroundColor) ] <|
                 html <|
                     MaterialIcons.save 28 Inherit
             , link []
                 { url = Route.toString Route.Top
                 , label =
-                    el [] <|
+                    el [ Font.color (Manifest.Color.contrast manifest.backgroundColor) ] <|
                         html <|
                             MaterialIcons.delete 28 Inherit
                 }
